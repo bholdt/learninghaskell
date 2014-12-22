de0cabef615770c1143ec345f1baa1d58396a7b0
Björn Holdt <bm.holdt@gmail.com>
2014-12-22 12:48:30 +1000

    Exercise 1- 4

---
+++ homework1.hs
@@ -0,0 +1,22 @@
+toDigits :: Integer -> [Integer]
+toDigitsRev :: Integer -> [Integer]
+toDigits n
+    | n <= 0 = []
+    | otherwise = toDigits (n `quot` 10) ++ [n `mod` 10]
+toDigitsRev n
+    | n <= 0 = []
+    | otherwise = n `mod` 10 : toDigitsRev (n `quot` 10)
+
+doubleEverySecond :: [Integer] -> [Integer]
+doubleEverySecond [] = []
+doubleEverySecond [x] = [x]
+doubleEverySecond (x:y:zs) = x : (y + y) : doubleEverySecond zs
+
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse(doubleEverySecond(reverse(xs)))
+
+main = do
+    print (toDigits(123))
+    print (toDigitsRev(123))
+    print (doubleEveryOther([8,7,6,5]))​
