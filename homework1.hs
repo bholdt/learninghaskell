83a8fa378013ffc7d8c1e47251a57af2f5cd7ea5
Björn Holdt <bm.holdt@gmail.com>
2014-12-22 13:06:23 +1000

    Sum Digits Exercise completed.

--- homework1.hs
+++ homework1.hs
@@ -15,5 +15,15 @@ doubleEverySecond (x:y:zs) = x : (y + y) : doubleEverySecond zs
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther xs = reverse . doubleEverySecond . reverse $ xs
 
-main = print $ doubleEveryOther [8,7,6,5]
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:ys) = sumDigit(x) + sumDigits(ys)
 
+sumDigit :: Integer -> Integer
+sumDigit n
+    | n <= 0 = 0
+    | otherwise = n `mod` 10 + sumDigit(n `quot` 10)
+
+main = do
+           print $ doubleEveryOther [8,7,6,5]
+           print $ sumDigits [12,3,11]
