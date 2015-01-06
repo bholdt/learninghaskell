7e1346651f241e93467ac563cf68f466f188c787
Björn Holdt <bm.holdt@gmail.com>
2015-01-06 16:50:35 +1000

    Start of solving hanoi problem

--- hanoi.hs
+++ hanoi.hs
@@ -1,1 +1,9 @@
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
+hanoi num a b c 
+                | num == 1 = [(a,c)]
+                | otherwise = hanoi (num - 1) a b c
 
+main = do
+           print $ hanoi 2 "a" "b" "c"
