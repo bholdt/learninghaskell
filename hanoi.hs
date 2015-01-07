40bcc3fad9978600e4076f2113b27a7ecca75afc
Björn Holdt <bm.holdt@gmail.com>
2015-01-07 14:34:02 +1000

    Hanoi solve attempt
    Not quite there.

--- hanoi.hs
+++ hanoi.hs
@@ -1,9 +1,9 @@
 type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
-hanoi num a b c 
-                | num == 1 = [(a,c)]
-                | otherwise = hanoi (num - 1) a b c
+hanoi num source spare destination 
+                | num == 0 = [(source,destination)]
+                | otherwise = hanoi (num - 1) spare source destination ++ [(source, destination)] ++ hanoi (num - 1) spare destination source
 
 main = do
-           print $ hanoi 2 "a" "b" "c"
+           print $ hanoi 3 "a" "b" "c"
